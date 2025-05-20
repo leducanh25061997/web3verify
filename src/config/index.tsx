c51e3d61af5e5e1e05a7f266032602493dfdc7d0
Waebox Media <waeboxmedia@192.168.1.24.non-exists.ptr.local>
2025-05-20 23:43:20 +0700

    update

---
+++ src/config/index.tsx
@@ -0,0 +1,28 @@
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
+import type { AppKitNetwork } from '@reown/appkit/networks'
+
+// Get projectId from https://cloud.reown.com
+export const projectId = "93ac67bd0ee56fd6beac77b419053d0c" // this is a public projectId only to use on localhost
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+export const metadata = {
+    name: 'AppKit',
+    description: 'AppKit Example',
+    url: 'https://reown.com', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/179229932']
+  }
+
+// for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
+export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+
+//Set up the Wagmi Adapter (Config)
+export const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks
+})
+
+export const config = wagmiAdapter.wagmiConfig
